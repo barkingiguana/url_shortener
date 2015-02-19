4f0c79b6b927da4c4226eaa5290380e4e0f505d1
Craig R Webster <craig@barkingiguana.com>
2015-02-19 11:34:00 +0000

    Example client + look up secret from database

--- config.ru
+++ config.ru
@@ -6,9 +6,15 @@ DB.loggers << Logger.new($stdout)
 DB.loggers.each { |l| l.level = Logger::DEBUG }
 DB.create_table? :links do
   primary_key :id
-  String :code
+  String :code # TODO: Add a unique constraint to this
   String :target
 end
+DB.create_table? :accounts do
+  primary_key :id
+  String :public_key # TODO: Add a unique constraint to this
+  String :secret
+end
+DB[:accounts].insert public_key: 'example', secret: 'Thi|niemC/ivah9_dEicaiwim3'
 $:.unshift File.expand_path '../lib', __FILE__
 require 'barking_iguana/url_shortener'
 run BarkingIguana::UrlShortener::Server::Service
--- lib/barking_iguana/url_shortener.rb
+++ lib/barking_iguana/url_shortener.rb
@@ -6,20 +6,23 @@ require 'base_n'
 
 module BarkingIguana
   module UrlShortener
-    SECRETS = {
-      "example".freeze => "VxucFV2ypDvQtp92ZRgZUcsvNYnd4rvtJcKAqyveJ94K9c24".freeze
-    }.freeze
-
     module Client
       class Client
+        attr_accessor :public_key, :secret, :base_url
+        def initialize public_key, secret, base_url: 'http://127.0.0.1:9292'
+          self.public_key = public_key
+          self.secret = secret
+          self.base_url = base_url
+        end
+
         def add target
           loop do
             now = BaseN::Number.new(Time.now.to_i.to_s, 10).rebase(58).to_s
             jitter = BaseN::Number.new(rand(1_000_000).to_s, 10).rebase(58).to_s
             code = "#{now}#{jitter}"
             intent = BarkingIguana::Verify::SignableAction.new 'PUT', "/#{code}", target: target
-            action = intent.sign SECRETS.keys[0], SECRETS.values[0], Time.now + 120
-            r = RestClient.put "http://127.0.0.1:9292#{action.signed_path.path}", action.signed_path.query_values
+            action = intent.sign public_key, secret, Time.now + 10
+            r = RestClient.put "#{base_url}#{action.signed_path.path}", action.signed_path.query_values
             return r.headers[:location] if r.code == 201
           end
         end
@@ -35,10 +38,11 @@ module BarkingIguana
         end
 
         def verify_signature! verb, path, request_params
-          poster = params.delete BarkingIguana::Verify::SignedAction::PARAMETER_PUBLIC_KEY.to_s
+          public_key = params.delete BarkingIguana::Verify::SignedAction::PARAMETER_PUBLIC_KEY.to_s
           signature = params.delete BarkingIguana::Verify::SignedAction::PARAMETER_SIGNATURE.to_s
           intent = BarkingIguana::Verify::SignableAction.new verb, path, request_params
-          intent.verify! signature, SECRETS[poster]
+          secret = DB[:accounts].where(public_key: public_key).get(:secret)
+          intent.verify! signature, secret
         end
 
         put '/:code' do
